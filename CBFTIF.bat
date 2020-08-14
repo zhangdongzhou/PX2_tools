a8767d091be50738a47d6ff9c9086dc6e62faf07
zhangdongzhou <tochozhang@gmail.com>
2020-08-14 16:45:25 -0500

    temp bat storage

--- CBFTIF.bat
+++
@@ -1,2 +0,0 @@
-PATH=C:\Users\gpd_user\AppData\Local\Continuum\anaconda3;C:\Users\gpd_user\AppData\Local\Continuum\anaconda3\Library\usr\bin;C:\Users\gpd_user\AppData\Local\Continuum\anaconda3\Library\bin;C:\Users\gpd_user\AppData\Local\Continuum\anaconda3\Scripts;C:\Users\gpd_user\AppData\Local\Continuum\anaconda3\bin;C:\Users\gpd_user\AppData\Local\Continuum\anaconda3\condabin;%PATH%
-conda deactivate & conda activate BMCXtal & python C:\PythonStuff\PX2_tools\CBFTIF.py
